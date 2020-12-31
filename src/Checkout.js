83d6b686e40bc8dd8027004ce95cc06af8c4a0d8
taruntiwaritt <35301186+taruntiwaritt@users.noreply.github.com>
2020-12-31 16:29:54 +0530

    adding homepage components

---
+++ src/Checkout.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Checkout.css";
+function Checkout() {
+  return <div className="checkout">Checkout comp</div>;
+}
+
+export default Checkout;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCUjFl_S5vVNwcJpZ6PQxVlCSbqx_W_mYs",
+  authDomain: "clone-b3059.firebaseapp.com",
+  projectId: "clone-b3059",
+  storageBucket: "clone-b3059.appspot.com",
+  messagingSenderId: "904067759657",
+  appId: "1:904067759657:web:1d522133f67dc784a3a638",
+};
